49719d83a0e91158dba54b98a0e622c248d82cb2
gogogadgetbioprocess <chrisrgrant@gmail.com>
2016-10-12 16:04:27 +0100

    new medium resolution plate for picking and plating out

--- microArch/factory/make_plate_library.go
+++ microArch/factory/make_plate_library.go
@@ -433,26 +433,26 @@ func makePlateLibrary() map[string]*wtype.LHPlate {
 	// Onewell SBS format Agarplate with colonies on riser (50ml agar) med res
 
 	bottomtype = wtype.LHWBFLAT
-	xdim = 2.0
-	ydim = 2.0
+	xdim = 3.0
+	ydim = 3.0
 	zdim = 14.0
 	bottomh = 1.0
 
-	wellxoffset = 2.25             // centre of well to centre of neighbouring well in x direction
-	wellyoffset = 2.25             //centre of well to centre of neighbouring well in y direction
+	wellxoffset = 3.375            // centre of well to centre of neighbouring well in x direction
+	wellyoffset = 3.375            //centre of well to centre of neighbouring well in y direction
 	xstart = -2.5                  // distance from top left side of plate to first well
 	ystart = -2.5                  // distance from top left side of plate to first well
 	zstart = riserheightinmm + 3.5 //5.5 // offset of bottom of deck to bottom of well
 
-	square = wtype.NewShape("box", "mm", 2, 2, 14)
+	square = wtype.NewShape("box", "mm", 3, 3, 14)
 	//func NewLHWell(platetype, plateid, crds, vunit string, vol, rvol float64, shape *Shape, bott int, xdim, ydim, zdim, bottomh float64, dunit string) *LHWell {
-	welltype = wtype.NewLHWell("1536flat", "", "", "ul", 31.25, 5, square, bottomtype, xdim, ydim, zdim, bottomh, "mm")
+	welltype = wtype.NewLHWell("768flat", "", "", "ul", 31.25, 5, square, bottomtype, xdim, ydim, zdim, bottomh, "mm")
 
 	//func NewLHPlate(platetype, mfr string, nrows, ncols int, height float64, hunit string, welltype *LHWell, wellXOffset, wellYOffset, wellXStart, wellYStart, wellZStart float64) *LHPlate {
 	// greiner one well with 50ml of agar in
-	plate = wtype.NewLHPlate("NewAgarplateforpicking1536_riser", "Unknown", 32, 48, 14, "mm", welltype, wellxoffset, wellyoffset, xstart, ystart, zstart)
+	plate = wtype.NewLHPlate("Agarplateforpicking768_riser", "Unknown", 24, 32, 14, "mm", welltype, wellxoffset, wellyoffset, xstart, ystart, zstart)
 	plates[plate.Type] = plate
-	plate = wtype.NewLHPlate("NewAgarplateforpicking1536_riser40", "Unknown", 32, 48, 14, "mm", welltype, wellxoffset, wellyoffset, xstart, ystart, zstart)
+	plate = wtype.NewLHPlate("Agarplateforpicking768_riser40", "Unknown", 24, 32, 14, "mm", welltype, wellxoffset, wellyoffset, xstart, ystart, zstart)
 	plates[plate.Type] = plate
 
 	/*

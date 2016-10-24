06fdf0b69e10ffb9bad08037b1f3e54c9eee18cb
gogogadgetbioprocess <chrisrgrant@gmail.com>
2016-10-24 14:05:28 +0100

    revert b88295f10bd048a53c6109d150773d65b1d8c2ec

--- microArch/scheduler/liquidhandling/liquidhandler.go
+++ microArch/scheduler/liquidhandling/liquidhandler.go
@@ -761,18 +761,14 @@ func OutputSetup(robot *liquidhandling.LHProperties) {
 	logger.Debug("Tipboxes: ")
 
 	for k, v := range robot.Tipboxes {
-		//		logger.Debug(fmt.Sprintf("%s %s: %s", k, robot.PlateIDLookup[k], v.Type))
-		_ = k
-		_ = v
+		logger.Debug(fmt.Sprintf("%s %s: %s", k, robot.PlateIDLookup[k], v.Type))
 	}
 
 	logger.Debug("Plates:")
 
 	for k, v := range robot.Plates {
 
-		_ = k
-		_ = v
-		//logger.Debug(fmt.Sprintf("%s %s: %s %s", k, robot.PlateIDLookup[k], v.PlateName, v.Type))
+		logger.Debug(fmt.Sprintf("%s %s: %s %s", k, robot.PlateIDLookup[k], v.PlateName, v.Type))
 
 		wtype.AutoExportPlateCSV(v.GetName()+".csv", v)
 

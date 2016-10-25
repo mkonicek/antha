19564425d64f7a29e3a4a32e4917420108e9a383
Michael Sadowski <m.sadowski@synthace.com>
2016-10-25 23:01:12 +0100

    And a print

--- microArch/scheduler/liquidhandling/liquidhandler.go
+++ microArch/scheduler/liquidhandling/liquidhandler.go
@@ -349,7 +349,7 @@ func (this *Liquidhandler) revise_volumes(rq *LHRequest) error {
 			}
 
 			//fmt.Println(p2, " ", p1)
-			fmt.Println("Plate ID Map: ", p2.ID, " --> ", p1.ID)
+			//fmt.Println("Plate ID Map: ", p2.ID, " --> ", p1.ID)
 
 			//	this.plateIDMap[p2.ID] = p1.ID
 		}

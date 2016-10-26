78bcda887087d5972580345795ae95cd2e16d90f
gogogadgetbioprocess <chrisrgrant@gmail.com>
2016-10-26 16:47:22 +0100

    prevent large volume tips touching bottom of pcrplate_skirted and pcrplate_cooler

--- microArch/driver/liquidhandling/makelhpolicy.go
+++ microArch/driver/liquidhandling/makelhpolicy.go
@@ -793,10 +793,10 @@ func MakeLVDNAMixPolicy() LHPolicy {
 
 func MakeHVOffsetPolicy() LHPolicy {
 	lvop := make(LHPolicy, 6)
-	lvop["ASPZOFFSET"] = 1.50
-	lvop["DSPZOFFSET"] = 1.50
-	lvop["POST_MIX_Z"] = 1.00
-	lvop["PRE_MIX_Z"] = 1.00
+	lvop["ASPZOFFSET"] = 2.50
+	lvop["DSPZOFFSET"] = 2.50
+	lvop["POST_MIX_Z"] = 2.50
+	lvop["PRE_MIX_Z"] = 2.50
 	lvop["DSPREFERENCE"] = 0
 	lvop["ASPREFERENCE"] = 0
 	lvop["POST_MIX_RATE"] = 37

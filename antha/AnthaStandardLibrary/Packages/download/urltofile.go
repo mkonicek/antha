6dfbd0720736581516fadb12dcb897067c97fa9f
gogogadgetbioprocess <chrisrgrant@gmail.com>
2016-12-19 15:27:23 +0000

    only return error not os.file

--- antha/AnthaStandardLibrary/Packages/download/urltofile.go
+++ antha/AnthaStandardLibrary/Packages/download/urltofile.go
@@ -9,24 +9,25 @@ import (
 
 // func File downloads a url to the given filename.
 // On error, the file will be left behind.
-func File(url string, filename string) (f *os.File, err error) {
+func File(url string, filename string) (err error) {
 
+	var f *os.File
 	res, err := http.Get(url)
 
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer res.Body.Close()
 
 	f, err = os.Create(filename)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer f.Close()
 
 	if _, err := io.Copy(f, res.Body); err != nil {
-		return nil, err
+		return err
 	}
 
-	return f, nil
+	return nil
 }

0474453bda850358f88f7f6b36554c20329ced9b
Michael Sadowski <m.sadowski@synthace.com>
2017-01-16 18:48:05 +0000

    Removed prints from test

--- microArch/factory/make_plate_library_test.go
+++ microArch/factory/make_plate_library_test.go
@@ -2,7 +2,6 @@
 package factory
 
 import (
-	"fmt"
 	"github.com/antha-lang/antha/antha/AnthaStandardLibrary/Packages/search"
 	"strings"
 	"testing"
@@ -20,7 +19,6 @@ var tests = []platetest{
 	platetest{TestPlateName: "greiner384", ExpectedZStart: 2.5, ExpectedHeight: 14.0},
 	platetest{TestPlateName: "Nuncon12wellAgar", ExpectedZStart: 9, ExpectedHeight: 22.0},
 }
-
 
 func TestAddRiser(t *testing.T) {
 
@@ -290,26 +288,17 @@ func TestGetConstraints(t *testing.T) {
 	}
 }
 
-
-
 func TestPlateZs(t *testing.T) {
-	allplates := GetPlateList()
-
-	for _, testplatename := range allplates {
-
-		testplate := GetPlateByType(testplatename)
-		fmt.Println("plate:", testplate.Type, "Z start", testplate.WellZStart)
-	}
 	for _, test := range tests {
 
 		testplate := GetPlateByType(test.TestPlateName)
-		
+
 		if testplate.WellZStart != test.ExpectedZStart {
-							t.Error(
-								"for", test.TestPlateName, "\n",
-								"expected height: ", test.ExpectedZStart, "\n",
-								"got height :", testplate.WellZStart, "\n",
-							)
-						}
+			t.Error(
+				"for", test.TestPlateName, "\n",
+				"expected height: ", test.ExpectedZStart, "\n",
+				"got height :", testplate.WellZStart, "\n",
+			)
+		}
 	}
 }

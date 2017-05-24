7ffd64cc8fdcfafaa546a3ec492191250def1f0c
n-scholes <n.scholes@synthace.com>
2017-05-24 14:47:18 +0100

    PCR renamed to pcr

--- antha/AnthaStandardLibrary/Packages/PCR/pcr.go
+++ antha/AnthaStandardLibrary/Packages/PCR/pcr.go
@@ -1,7 +1,7 @@
 // This is a package that contains types and function used for PCR reactions
-package PCR
+package pcr
 
-type PCRReaction struct {
+type Reaction struct {
 	ReactionName string
 	Template     string
 	PrimerPair   [2]string

d4340f4788f73b4514d2883c332106ccd516b3ce
n-scholes <n.scholes@synthace.com>
2017-05-26 13:49:50 +0100

    PCR parser updates to handle DNAsequence rather than string in type Reaction

--- antha/AnthaStandardLibrary/Packages/PCR/pcr.go
+++
@@ -1,8 +0,0 @@
-// This is a package that contains types and function used for PCR reactions
-package pcr
-
-type Reaction struct {
-	ReactionName string
-	Template     string
-	PrimerPair   [2]string
-}

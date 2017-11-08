01c1601f473ce23011808e8141adde6f0780fd05
Michael Sadowski <m.sadowski@synthace.com>
2017-11-08 16:49:54 +0000

    Add end reverse-complement check

--- antha/AnthaStandardLibrary/Packages/Optimization/concreteOptimizer.go
+++ antha/AnthaStandardLibrary/Packages/Optimization/concreteOptimizer.go
@@ -352,7 +352,7 @@ func makeSplits(seq string, p, endLen int, endsToAvoid []string) []string {
 	r := make([]string, 0, endLen+1)
 	for i := 0; i < 1; i++ {
 		end := string(seq[p+i-1 : p+endLen-1])
-		if isIn(end, endsToAvoid) {
+		if isIn(end, endsToAvoid) || isIn(wtype.RevComp(end), endsToAvoid) {
 			continue
 		}
 		r = append(r, end)

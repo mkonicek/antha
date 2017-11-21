d30734a30fbeee39b5959599e4157c98179162a8
Michael Sadowski <m.sadowski@synthace.com>
2017-11-21 15:31:26 +0000

    Fix volume pass through

--- microArch/scheduler/liquidhandling/fixvolumes.go
+++ microArch/scheduler/liquidhandling/fixvolumes.go
@@ -3,6 +3,7 @@ package liquidhandling
 import (
 	"github.com/antha-lang/antha/antha/anthalib/wtype"
 	"github.com/antha-lang/antha/antha/anthalib/wunit"
+	"strings"
 )
 
 func FixVolumes(request *LHRequest) (*LHRequest, error) {
@@ -11,16 +12,58 @@ func FixVolumes(request *LHRequest) (*LHRequest, error) {
 	wantedVolumes := make(map[string]wunit.Volume)
 	c := 0
 	for chainEnd := findChainEnd(request.InstructionChain); chainEnd != nil; chainEnd = chainEnd.Parent {
-		stageVolumes, err := findUpdateInstructionVolumes(chainEnd, wantedVolumes, request.MergedInputOutputPlates())
+		if len(chainEnd.Values) > 0 && chainEnd.Values[0].Type == wtype.LHIMIX {
+			stageVolumes, err := findUpdateInstructionVolumes(chainEnd, wantedVolumes, request.MergedInputOutputPlates())
 
-		if err != nil {
-			return request, err
+			if err != nil {
+				return request, err
+			}
+			wantedVolumes = stageVolumes
+		} else if chainEnd.Values[0].Type == wtype.LHIPRM {
+			// update the wanted volumes to the correct names
+			wantedVolumes = passThrough(chainEnd.Values, wantedVolumes)
 		}
-		wantedVolumes = stageVolumes
 		c += 1
 	}
 
 	return request, nil
+}
+
+func passThrough(values []*wtype.LHInstruction, wanted map[string]wunit.Volume) map[string]wunit.Volume {
+	ret := make(map[string]wunit.Volume, len(wanted))
+
+	for _, v := range values {
+		ret = passThroughMap(v, wanted, ret)
+	}
+
+	return ret
+}
+
+func passThroughMap(ins *wtype.LHInstruction, wanted, updated map[string]wunit.Volume) map[string]wunit.Volume {
+	if ins.Type == wtype.LHIPRM {
+		for IDin, out := range ins.PassThrough {
+			IDout := out.ID
+
+			vol, ok := getWantVol(wanted, out.FullyQualifiedName())
+
+			if ok {
+				newName := strings.Replace(out.FullyQualifiedName(), IDout, IDin, -1)
+				if wantInPlace(wanted, out.FullyQualifiedName()) {
+					newName = newName + wtype.InPlaceMarker
+				}
+				updated[newName] = vol
+				deleteWantOf(wanted, out.FullyQualifiedName())
+			}
+		}
+
+	}
+	// merge in remaining keys from wanted
+
+	for k, v := range wanted {
+		updated[k] = v
+	}
+
+	return updated
 }
 
 func findUpdateInstructionVolumes(ch *IChain, wanted map[string]wunit.Volume, plates map[string]*wtype.LHPlate) (map[string]wunit.Volume, error) {

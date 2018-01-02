a57101ea3c7401accf7a52c60d7ae21f77dc2d81
ddn0 <d.nguyen@synthace.com>
2018-01-02 17:33:38 +0000

    jfile: test for larger uploads

--- antha/AnthaStandardLibrary/Packages/jfile/jfile_test.go
+++ antha/AnthaStandardLibrary/Packages/jfile/jfile_test.go
@@ -3,11 +3,89 @@ package jfile
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"testing"
 	"time"
 )
+
+// Spin waiting for file to appear
+func spinUntil(ctx context.Context, c *Client, filename string, seconds int) error {
+	for i := 0; i < seconds; i++ {
+		<-time.After(1 * time.Second)
+		files, err := c.ListFiles(ctx, "")
+		if err != nil {
+			return err
+		}
+		for _, f := range files {
+			if f.Name == filename {
+				return nil
+			}
+		}
+	}
+
+	return fmt.Errorf("file not found")
+}
+
+func TestLargeFiles(t *testing.T) {
+	if len(os.Getenv("ANTHA_PASSWORD")) == 0 {
+		t.Skip("missing required environment variables")
+	}
+
+	const (
+		filename  = "test_large_file"
+		kilobytes = 1000 * 10 // 10 MB
+	)
+
+	c, err := DefaultClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := context.Background()
+
+	w := c.NewWriter(ctx, filename)
+	defer w.Close() // nolint
+
+	inR, inW := io.Pipe()
+
+	go func() {
+		defer inW.Close()
+		var buf [1024]byte
+		for i := 0; i < kilobytes; i++ {
+			if _, err := inW.Write(buf[:]); err != nil {
+				inW.CloseWithError(err)
+				return
+			}
+		}
+	}()
+
+	if _, err := io.Copy(w, inR); err != nil {
+		t.Error(err)
+	}
+
+	if err := w.Close(); err != nil {
+		t.Error(err)
+	}
+
+	if err := spinUntil(ctx, c, filename, 5); err != nil {
+		t.Fatal(err)
+	}
+
+	r := c.NewReader(ctx, "", filename)
+	defer r.Close() // nolint
+
+	n, err := io.Copy(ioutil.Discard, r)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if e, f := 1024*int64(kilobytes), n; e != f {
+		t.Errorf("expecting %d found %d", e, f)
+	}
+}
 
 func TestDefaultClient(t *testing.T) {
 	if len(os.Getenv("ANTHA_PASSWORD")) == 0 {
@@ -53,24 +131,8 @@ func TestDefaultClient(t *testing.T) {
 		t.Error(err)
 	}
 
-	// Spin waiting for file to appear
-	found := false
-	for i := 0; i < 5 && !found; i++ {
-		<-time.After(1 * time.Second)
-		files, err := c.ListFiles(ctx, "")
-		if err != nil {
-			t.Fatal(err)
-		}
-		for _, f := range files {
-			if f.Name == filename {
-				found = true
-				break
-			}
-		}
-	}
-
-	if !found {
-		t.Fatalf("file not found")
+	if err := spinUntil(ctx, c, filename, 5); err != nil {
+		t.Fatal(err)
 	}
 
 	r := c.NewReader(ctx, "", filename)

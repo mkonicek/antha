7b68154dd589a03f21f3ca3a06beaf57a5044d41
Michael Sadowski <m.sadowski@synthace.com>
2018-01-04 17:45:18 +0000

    Protobuf definition for plate reader (write-only)

--- driver/driver.go
+++ driver/driver.go
@@ -2,6 +2,7 @@
 //go:generate protoc -I${GOPATH}/src ${GOPATH}/src/github.com/antha-lang/antha/driver/antha_driver_v1/driver.proto --go_out=plugins=grpc:${GOPATH}/src
 //go:generate protoc -I${GOPATH}/src ${GOPATH}/src/github.com/antha-lang/antha/driver/antha_shakerincubator_v1/shakerincubator.proto --go_out=plugins=grpc:${GOPATH}/src
 //go:generate protoc -I${GOPATH}/src ${GOPATH}/src/github.com/antha-lang/antha/driver/antha_human_v1/human.proto --go_out=plugins=grpc:${GOPATH}/src
+//go:generate protoc -I${GOPATH}/src ${GOPATH}/src/github.com/antha-lang/antha/driver/antha_platereader_v1/platereader.proto --go_out=plugins=grpc:${GOPATH}/src
 //go:generate protoc -I. lh/lh.proto --go_out=plugins=grpc:pb
 
 package driver

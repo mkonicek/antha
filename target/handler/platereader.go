32e1b262db1ab53a3427470d5c78f598b9468dc0
Michael Sadowski <m.sadowski@synthace.com>
2018-01-11 15:40:31 +0000

    added comment to public method

--- target/handler/platereader.go
+++ target/handler/platereader.go
@@ -5,6 +5,7 @@ import (
 	"github.com/antha-lang/antha/target"
 )
 
+// Makes a new handle-based write-only plate reader
 func NewWOPlateReader() *Handler {
 	return New([]ast.NameValue{target.DriverSelectorV1WriteOnlyPlateReader})
 }

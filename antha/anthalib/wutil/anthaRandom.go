7f9ef962339ecbce3c178a940b7d4e5b81a3ca04
Michael Sadowski <m.sadowski@synthace.com>
2018-01-22 14:40:30 +0000

    Comments for anthaRandom

--- antha/anthalib/wutil/anthaRandom.go
+++ antha/anthalib/wutil/anthaRandom.go
@@ -9,11 +9,15 @@ import (
 
 var ourRand *rand.Rand
 
-func GetRandomWithSeed(seed int64) *rand.Rand {
-	ourRand = rand.New(rand.NewSource(seed))
+// GetRandomWithSeed returns a random number generator, seeded with the seed provided if requested
+func GetRandomWithSeed(seed int64, reseed bool) *rand.Rand {
+	if reseed || ourRand == nil {
+		ourRand = rand.New(rand.NewSource(seed))
+	}
 	return ourRand
 }
 
+// GetRandom returns a random number generator, seeding it if it is not initialised
 func GetRandom() *rand.Rand {
 	if ourRand == nil {
 		ourRand = rand.New(rand.NewSource(time.Now().Unix()))

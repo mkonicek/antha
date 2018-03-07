7d4c286f98bba1afc6c97ba6a139f6a04fc2ce57
chr1sgrant <chrisrgrant@gmail.com>
2018-03-07 11:30:42 +0000

    gofmt

--- cmd/antha/cmd/convert_to_ui.go
+++ cmd/antha/cmd/convert_to_ui.go
@@ -51,9 +51,9 @@ type uiMixerConfig struct {
 	MaxPlates                         float64  `json:"maxPlates,omitempty"`
 	MaxWells                          float64  `json:"maxWells,omitempty"`
 	ResidualVolumeWeight              float64  `json:"residualVolumeWeight,omitempty"`
-	InputPlateTypes                    []string `json:"inputPlateTypes"`
-	OutputPlateTypes                   []string `json:"outputPlateTypes"`
-	TipTypes                           []string `json:"tipTypes"`
+	InputPlateTypes                   []string `json:"inputPlateTypes"`
+	OutputPlateTypes                  []string `json:"outputPlateTypes"`
+	TipTypes                          []string `json:"tipTypes"`
 	PlanningVersion                   string   `json:"planningVersion"`
 	DriverSpecificInputPreferences    []string `json:"driverSpecificInputPreferences"`
 	DriverSpecificOutputPreferences   []string `json:"driverSpecificOutputPreferences"`
@@ -90,9 +90,9 @@ func convertConfigToUI(in *mixer.Opt) *uiMixerConfig {
 		MaxPlates:                         getFloat(in.MaxPlates),
 		MaxWells:                          getFloat(in.MaxWells),
 		ResidualVolumeWeight:              getFloat(in.ResidualVolumeWeight),
-		InputPlateTypes:                    getSlice(in.InputPlateTypes),
-		OutputPlateTypes:                   getSlice(in.OutputPlateTypes),
-		TipTypes:                           getSlice(in.TipTypes),
+		InputPlateTypes:                   getSlice(in.InputPlateTypes),
+		OutputPlateTypes:                  getSlice(in.OutputPlateTypes),
+		TipTypes:                          getSlice(in.TipTypes),
 		PlanningVersion:                   in.PlanningVersion,
 		DriverSpecificInputPreferences:    getSlice(in.DriverSpecificInputPreferences),
 		DriverSpecificOutputPreferences:   getSlice(in.DriverSpecificOutputPreferences),

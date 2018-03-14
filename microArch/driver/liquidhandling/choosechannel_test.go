915089c26bc52a2a67c04bb096e53db77812432f
Haydn King <h.king@synthace.com>
2018-03-14 11:09:47 +0000

    Ignore DF30 filter tips during test

--- microArch/driver/liquidhandling/choosechannel_test.go
+++ microArch/driver/liquidhandling/choosechannel_test.go
@@ -170,7 +170,7 @@ func SetUpTipsFor(lhp *LHProperties) *LHProperties {
 	for _, tb := range testinventory.GetTipboxes(ctx) {
 		if tb.Mnfr == lhp.Mnfr || lhp.Mnfr == "MotherNature" {
 			//ignore filter tips
-			if tb.Type == "DF200 Tip Rack (PIPETMAX 8x200)" || tb.Type == "DFL10 Tip Rack (PIPETMAX 8x20)" {
+			if tb.Type == "DF200 Tip Rack (PIPETMAX 8x200)" || tb.Type == "DFL10 Tip Rack (PIPETMAX 8x20)" || tb.Type == "DF30 Tip Rack (PIPETMAX 8x20)" {
 				continue
 			}
 			tip := tb.Tips[0][0]

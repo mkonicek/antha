537931d55d617d4ebb72665d7f9445a6df84b2a7
chr1sgrant <chrisrgrant@gmail.com>
2018-04-20 13:41:31 +0100

    add test case where component is on plate

--- antha/AnthaStandardLibrary/Packages/search/plates_test.go
+++ antha/AnthaStandardLibrary/Packages/search/plates_test.go
@@ -33,7 +33,7 @@ import (
 	"testing"
 
 	"github.com/antha-lang/antha/antha/anthalib/wtype"
-
+	"github.com/antha-lang/antha/antha/anthalib/wunit"
 	"github.com/antha-lang/antha/inventory"
 	"github.com/antha-lang/antha/inventory/testinventory"
 )
@@ -53,6 +53,7 @@ func TestNextFreeWell(t *testing.T) {
 		expectedError  error
 	}
 
+	// create a test plate
 	falconAgarPlate, err := inventory.NewPlate(defaultContext(), "falcon6wellAgar")
 
 	if err != nil {
@@ -61,6 +62,34 @@ func TestNextFreeWell(t *testing.T) {
 
 	falconAgarPlate.SetName("Agar plate")
 
+	falconAgarPlateWithSomethingIn, err := inventory.NewPlate(defaultContext(), "falcon6wellAgar")
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	// second test plate which we'll add a sample to.
+	falconAgarPlateWithSomethingIn.SetName("Agar plate")
+
+	component, err := inventory.NewComponent(defaultContext(), "water")
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	component.SetName("test_sample")
+
+	component.SetVolume(wunit.NewVolume(100.0, "ul"))
+
+	// this will add the component to the plate
+	_, err = falconAgarPlateWithSomethingIn.AddComponent(component, false)
+
+	if err != nil {
+		t.Error(
+			err.Error(),
+		)
+	}
+
 	var nextwellTests = []nextWellTest{
 		{
 			avoidWells:     []string{},
@@ -68,6 +97,14 @@ func TestNextFreeWell(t *testing.T) {
 			plateType:      falconAgarPlate,
 			byRow:          false,
 			expectedResult: "A1",
+			expectedError:  nil,
+		},
+		{
+			avoidWells:     []string{},
+			preferredWells: []string{},
+			plateType:      falconAgarPlateWithSomethingIn,
+			byRow:          false,
+			expectedResult: "B1",
 			expectedError:  nil,
 		},
 		{
@@ -133,7 +170,7 @@ func TestNextFreeWell(t *testing.T) {
 
 		if well != test.expectedResult {
 			t.Error(
-				"For", test.plateType.Type, test.avoidWells, test.preferredWells, test.byRow, "\n",
+				"For", test.plateType, test.avoidWells, test.preferredWells, test.byRow, "\n",
 				"expected:", test.expectedResult, "\n",
 				"got", well, "\n",
 			)
@@ -150,7 +187,7 @@ func TestNextFreeWell(t *testing.T) {
 				}
 			} else {
 				t.Error(
-					"For", test.plateType.Type, test.avoidWells, test.preferredWells, test.byRow, "\n",
+					"For", test.plateType, test.avoidWells, test.preferredWells, test.byRow, "\n",
 					"expected:", test.expectedError, "\n",
 					"got", err, "\n",
 				)

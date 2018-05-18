7e86dc29c77e531bb3ab65aafa036aba51f5bf78
chr1sgrant <chrisrgrant@gmail.com>
2018-05-18 13:03:14 +0100

    add example use case for method

--- antha/anthalib/wtype/lhcomponent.go
+++ antha/anthalib/wtype/lhcomponent.go
@@ -473,8 +473,10 @@ func (lhc *LHComponent) SetPolicyName(policy PolicyName) error {
 
 // ModifyLHPolicyParameter specifies that this LHComponent or instance of the LHComponent should be handled with a modified
 // LHPolicy parameter.
+// e.g. to Change number of post mixes to 5:
+// lhc.ModifyLHPolicyParameter("POST_MIX", 5)
 // Valid parameters and value types are specified in aparam.go
-// An error is returned if an invalid parameter or value type for that parameter are specified.
+// An error is returned if an invalid parameter or value type for that parameter is specified.
 func (lhc *LHComponent) ModifyLHPolicyParameter(parameter string, value interface{}) error {
 	if lhc.Policy == nil || len(lhc.Policy) == 0 {
 		lhc.Policy = make(LHPolicy)

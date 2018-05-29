be0d711592309d395bc766ed6e85b053e8be48e4
Michael Sadowski <m.sadowski@synthace.com>
2018-05-29 13:03:25 +0100

    First tranche of tests

--- microArch/scheduler/liquidhandling/input_plate_linear_test.go
+++ microArch/scheduler/liquidhandling/input_plate_linear_test.go
@@ -1,14 +1,64 @@
 package liquidhandling
 
 import (
+	"context"
 	"fmt"
 	"github.com/antha-lang/antha/antha/anthalib/wtype"
 	"github.com/antha-lang/antha/antha/anthalib/wunit"
+	"github.com/antha-lang/antha/inventory"
+	"github.com/antha-lang/antha/inventory/cache/plateCache"
+	"github.com/antha-lang/antha/inventory/testinventory"
 	"testing"
 )
 
+func summarize(m map[string]map[*wtype.LHPlate]int) map[string]map[string]int {
+	r := make(map[string]map[string]int, len(m))
+
+	for k, v := range m {
+		mm := make(map[string]int, len(v))
+
+		for kk, vv := range v {
+			mm[kk.Type] = vv
+		}
+
+		r[k] = mm
+	}
+
+	return r
+}
+
 // choose_plate_assignments(component_volumes map[string]wunit.Volume, plate_types []*wtype.LHPlate, weight_constraint map[string]float64) (map[string]map[*wtype.LHPlate]int, error)
 func TestIPL1(t *testing.T) {
+	// a few plates
+
+	ctx := testinventory.NewContext(context.Background())
+	ctx = plateCache.NewContext(ctx)
+
+	pcrplateSkirted, err := inventory.NewPlate(ctx, "pcrplate_skirted")
+
+	if err != nil {
+		t.Errorf("Can't get pcrplate_skirted")
+	}
+
+	/*
+		DSW96, err := inventory.NewPlate(ctx, "DSW96")
+		if err != nil {
+			t.Errorf("Can't get DSW96")
+		}
+
+		DWST12, err := inventory.NewPlate(ctx, "DWST12")
+		if err != nil {
+			t.Errorf("Can't get DWST12")
+		}
+	*/
+
+	reservoir, err := inventory.NewPlate(ctx, "reservoir")
+	if err != nil {
+		t.Errorf("Can't get reservoir")
+	}
+
+	weightConstraint := map[string]float64{"MAX_N_PLATES": 2.5, "MAX_N_WELLS": 128, "RESIDUAL_VOLUME_WEIGHT": 1.0}
+
 	type testCase struct {
 		Name              string
 		Component_volumes map[string]wunit.Volume
@@ -19,24 +69,66 @@ func TestIPL1(t *testing.T) {
 
 	testCases := []testCase{
 		{
-			Name:              "",
-			Component_volumes: nil,
-			Plate_types:       nil,
-			Weight_constraint: nil,
-			Expected:          nil,
+			Name:              "1thing1well",
+			Component_volumes: map[string]wunit.Volume{"water": wunit.NewVolume(50, "ul")},
+			Plate_types:       []*wtype.LHPlate{pcrplateSkirted},
+			Weight_constraint: weightConstraint,
+			Expected:          map[string]map[string]int{"water": {"pcrplate_skirted": 1}},
+		},
+		{
+			Name:              "1thing3wells",
+			Component_volumes: map[string]wunit.Volume{"water": wunit.NewVolume(500, "ul")},
+			Plate_types:       []*wtype.LHPlate{pcrplateSkirted},
+			Weight_constraint: weightConstraint,
+			Expected:          map[string]map[string]int{"water": {"pcrplate_skirted": 3}},
+		},
+		{
+			Name:              "1thing2plates",
+			Component_volumes: map[string]wunit.Volume{"water": wunit.NewVolume(18915, "ul")},
+			Plate_types:       []*wtype.LHPlate{pcrplateSkirted},
+			Weight_constraint: weightConstraint,
+			Expected:          map[string]map[string]int{"water": {"pcrplate_skirted": 97}},
+		},
+		{
+			Name:              "failWellConstraint129wells",
+			Component_volumes: map[string]wunit.Volume{"water": wunit.NewVolume(25155, "ul")},
+			Plate_types:       []*wtype.LHPlate{pcrplateSkirted},
+			Weight_constraint: weightConstraint,
+			Expected:          map[string]map[string]int{"FAIL": {}},
+		},
+		{
+			Name:              "failPlateConstraint3plates",
+			Component_volumes: map[string]wunit.Volume{"water": wunit.NewVolume(20000, "ul"), "scotch": wunit.NewVolume(20000, "ul"), "consomme": wunit.NewVolume(20000, "ul"), "soup": wunit.NewVolume(20000, "ul")},
+			Plate_types:       []*wtype.LHPlate{reservoir},
+			Weight_constraint: weightConstraint,
+			Expected:          map[string]map[string]int{"FAIL": {}},
 		},
 	}
 
 	for _, testCase := range testCases {
 		doTheTest := func(t *testing.T) {
+			fmt.Println("AHMA DOIN MA TEST ", testCase.Name)
 			results, err := choose_plate_assignments(testCase.Component_volumes, testCase.Plate_types, testCase.Weight_constraint)
 
-			if err != nil {
-				t.Errorf("Expected %s got error %v", fmtIt(testCase.Expected), err.Error())
-			}
+			_, fail := testCase.Expected["FAIL"]
 
-			if !compare(results, testCase.Expected) {
-				t.Errorf("Expected: %s got %s", fmtIt(testCase.Expected), fmtIt(results))
+			if fail {
+				// routine should fail
+
+				if err == nil {
+					t.Errorf("Expected failure, got %v", summarize(results))
+				}
+
+			} else {
+				// routine should pass
+
+				if err != nil {
+					t.Errorf("Expected %s got error %v", fmtIt(testCase.Expected), err.Error())
+				}
+
+				if !compare(results, testCase.Expected) {
+					t.Errorf("Expected: %s got %s", fmtIt(testCase.Expected), fmtIt(results))
+				}
 			}
 		}
 

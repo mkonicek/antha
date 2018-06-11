cb26d6ee520dd468fb735800c799c3ca36679fce
Haydn King <h.king@synthace.com>
2018-06-11 17:32:19 +0100

    Add tipbox tests

--- antha/anthalib/wtype/lhtipbox_test.go
+++ antha/anthalib/wtype/lhtipbox_test.go
@@ -5,6 +5,14 @@ import (
 	"testing"
 )
 
+func maketipboxfortest() *LHTipbox {
+	shp := NewShape("cylinder", "mm", 7.3, 7.3, 51.2)
+	w := NewLHWell("ul", 250.0, 10.0, shp, FlatWellBottom, 7.3, 7.3, 51.2, 0.0, "mm")
+	tiptype := NewLHTip("me", "mytype", 0.5, 1000.0, "ul", false, shp)
+	tb := NewLHTipbox(8, 12, Coordinates{127.76, 85.48, 120.0}, "me", "mytype", tiptype, w, 9.0, 9.0, 0.5, 0.5, 0.0)
+	return tb
+}
+
 func TestInflateMask(t *testing.T) {
 	m := []bool{true}
 
@@ -123,4 +131,66 @@ func TestTrimToMask(t *testing.T) {
 		t.Errorf("Expected %v, got %v", expected, trimmed)
 	}
 
+}
+
+func TestTipboxWellCoordsToCoords(t *testing.T) {
+
+	tb := maketipboxfortest()
+
+	pos, ok := tb.WellCoordsToCoords(MakeWellCoords("A1"), BottomReference)
+	if !ok {
+		t.Fatal("well A1 doesn't exist!")
+	}
+
+	xExpected := tb.TipXStart
+	yExpected := tb.TipYStart
+
+	if pos.X != xExpected || pos.Y != yExpected {
+		t.Errorf("position was wrong: expected (%f, %f) got (%f, %f)", xExpected, yExpected, pos.X, pos.Y)
+	}
+
+}
+
+func TestTipboxCoordsToWellCoords(t *testing.T) {
+
+	tb := maketipboxfortest()
+
+	pos := Coordinates{
+		X: tb.TipXStart + 0.75*tb.TipXOffset,
+		Y: tb.TipYStart + 0.75*tb.TipYOffset,
+	}
+
+	wc, delta := tb.CoordsToWellCoords(pos)
+
+	if e, g := "B2", wc.FormatA1(); e != g {
+		t.Errorf("Wrong well coordinates: expected %s, got %s", e, g)
+	}
+
+	eDelta := -0.25 * tb.TipXOffset
+	if delta.X != eDelta || delta.Y != eDelta {
+		t.Errorf("Delta incorrect: expected (%f, %f), got (%f, %f)", eDelta, eDelta, delta.X, delta.Y)
+	}
+
+}
+
+func TestTipboxGetWellBounds(t *testing.T) {
+
+	tb := maketipboxfortest()
+
+	eStart := Coordinates{
+		X: 0.5 - 0.5*8.2,
+		Y: 0.5 - 0.5*8.2,
+		Z: 0.5,
+	}
+	eSize := Coordinates{
+		X: 9.0*11 + 8.2,
+		Y: 9.0*7 + 8.2,
+		Z: 41.3,
+	}
+	eBounds := NewBBox(eStart, eSize)
+	bounds := tb.GetTipBounds()
+
+	if e, g := eBounds.String(), bounds.String(); e != g {
+		t.Errorf("GetWellBounds incorrect: expected %v, got %v", eBounds, bounds)
+	}
 }

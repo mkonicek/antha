17d06f6128a036893dfd80209b5cbd4acb062d71
Haydn King <h.king@synthace.com>
2018-06-25 11:07:34 +0100

    Remove USE_LLF config, add USE_LLF Policy

--- microArch/driver/liquidhandling/compositerobotinstruction.go
+++ microArch/driver/liquidhandling/compositerobotinstruction.go
@@ -1767,7 +1767,7 @@ func (ins *SuckInstruction) Generate(ctx context.Context, policy *wtype.LHPolicy
 	final_asp_ref := SafeGetInt(pol, "ASPREFERENCE")
 
 	//LLF
-	use_llf, any_llf := get_use_llf(policy, ins.Multi, ins.PltFrom, prms)
+	use_llf, any_llf := get_use_llf(pol, ins.Multi, ins.PltFrom, prms)
 	if any_llf {
 		below_surface := SafeGetF64(pol, "LLFBELOWSURFACE")
 		//Is the liquid height in each well higher than below_surface
@@ -2214,7 +2214,7 @@ func (ins *BlowInstruction) Generate(ctx context.Context, policy *wtype.LHPolicy
 	defaultspeed := SafeGetF64(pol, "DEFAULTZSPEED")
 
 	//LLF
-	use_llf, any_llf := get_use_llf(policy, ins.Multi, ins.PltTo, prms)
+	use_llf, any_llf := get_use_llf(pol, ins.Multi, ins.PltTo, prms)
 	if any_llf {
 		//override reference
 		ref = 2 //liquid level
@@ -3526,10 +3526,10 @@ func getMulti(w []string) int {
 	return c
 }
 
-func get_use_llf(policy *wtype.LHPolicyRuleSet, multi int, plates []string, prms *LHProperties) ([]bool, bool) {
+func get_use_llf(pol wtype.LHPolicy, multi int, plates []string, prms *LHProperties) ([]bool, bool) {
 	use_llf := make([]bool, multi)
 	any_llf := false
-	enable_llf := SafeGetBool(policy.Options, "USE_LLF")
+	enable_llf := SafeGetBool(pol, "USE_LLF")
 
 	//save a few ms
 	if !enable_llf {

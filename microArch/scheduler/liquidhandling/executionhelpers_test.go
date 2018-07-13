b3a095644a1b04326bbf6a8350564ffed6064056
Haydn King <h.king@synthace.com>
2018-07-13 13:33:00 +0100

    move old test over to new test runner

--- microArch/scheduler/liquidhandling/executionhelpers_test.go
+++ microArch/scheduler/liquidhandling/executionhelpers_test.go
@@ -35,6 +35,21 @@ func GetSplitForTest(id string, input *wtype.Liquid, volume float64) (*wtype.LHI
 	return split, moving, remaining
 }
 
+func GetPromptForTest(message string, inputs ...*wtype.Liquid) (*wtype.LHInstruction, []*wtype.Liquid) {
+	ret := wtype.NewLHPromptInstruction()
+	ret.ID = message //ID will be overwritten, set the message as well for testing
+	ret.Message = message
+	for _, input := range inputs {
+		output := input.Cp()
+		output.ParentID = input.ID
+		input.DaughterID = output.ID
+		ret.AddComponent(input)
+		ret.AddResult(output)
+	}
+
+	return ret, ret.Results
+}
+
 func GetLiquidForTest(name string, volume float64) *wtype.Liquid {
 	ret := wtype.NewLHComponent()
 	ret.CName = name
@@ -69,9 +84,19 @@ func (self *setOutputOrderTest) Run(t *testing.T) {
 	if e, g := len(self.ExpectedOrder), len(rq.Output_order); e != g {
 		t.Fatalf("Expected Order length mismatch:\n\te: %v\n\tg: %v", self.ExpectedOrder, rq.Output_order)
 	}
+
+	outputIDs := make([]string, 0, len(rq.Output_order))
+	for _, id := range rq.Output_order {
+		//for promts check the message as the ID is overwritten
+		if ins, ok := rq.LHInstructions[id]; ok && ins.Type == wtype.LHIPRM { //LHIPRM == prompt instruction
+			id = ins.Message
+		}
+		outputIDs = append(outputIDs, id)
+	}
+
 	for i := range self.ExpectedOrder {
-		if e, g := self.ExpectedOrder[i], rq.Output_order[i]; e != g {
-			t.Fatalf("Expected Order mismatch in item %d:\n\te: %v\n\tg: %v", i, self.ExpectedOrder, rq.Output_order)
+		if e, g := self.ExpectedOrder[i], outputIDs[i]; e != g {
+			t.Fatalf("Expected Order mismatch in item %d:\n\te: %v\n\tg: %v", i, self.ExpectedOrder, outputIDs)
 		}
 	}
 
@@ -133,6 +158,50 @@ func TestSetOutputOrdering_SplitMixes2(t *testing.T) {
 		OutputSort:    true,
 		ExpectedOrder: []string{"mixSquash", "mixShot", "theSplit", "mixLumpy"},
 		ChainHeight:   4,
+	}
+
+	test.Run(t)
+}
+
+func TestSetOutputOrder_Prompt(t *testing.T) {
+	// we go mix, prompt, split, mix
+	var instructions []*wtype.LHInstruction
+	var expectedIDOrder []string
+
+	c1 := GetLiquidForTest("water", 10.0)
+	c2 := GetLiquidForTest("washBuffer", 20.0)
+	mix1, c3 := GetMixForTest("mix1", c1, c2)
+
+	expectedIDOrder = append(expectedIDOrder, mix1.ID)
+	instructions = append(instructions, mix1)
+
+	prompt, promptResults := GetPromptForTest("prompt1", c3)
+	c4 := promptResults[0]
+
+	expectedIDOrder = append(expectedIDOrder, prompt.ID)
+	instructions = append(instructions, prompt)
+
+	split, c5, c4a := GetSplitForTest("split", c4, 10.0)
+	instructions = append(instructions, split)
+
+	c6 := GetLiquidForTest("turps", 100.0)
+	mix2, _ := GetMixForTest("mix2", c6, c5)
+
+	expectedIDOrder = append(expectedIDOrder, mix2.ID)
+	expectedIDOrder = append(expectedIDOrder, split.ID)
+	instructions = append(instructions, mix2)
+
+	// mix the static component with some more water
+	c7 := GetLiquidForTest("water", 200.0)
+	mix3, _ := GetMixForTest("mix3", c4a, c7)
+
+	expectedIDOrder = append(expectedIDOrder, mix3.ID)
+	instructions = append(instructions, mix3)
+
+	test := &setOutputOrderTest{
+		Instructions:  instructions,
+		ExpectedOrder: expectedIDOrder,
+		ChainHeight:   5,
 	}
 
 	test.Run(t)

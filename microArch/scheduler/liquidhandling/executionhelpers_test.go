4fb3b1d58de42d61bcbb1382cdc250e419c90a9a
Haydn King <h.king@synthace.com>
2018-07-20 11:39:13 +0100

    fix output order test

--- microArch/scheduler/liquidhandling/executionhelpers_test.go
+++ microArch/scheduler/liquidhandling/executionhelpers_test.go
@@ -76,7 +76,7 @@ func (self *setOutputOrderTest) Run(t *testing.T) {
 
 	rq.Options.OutputSort = self.OutputSort
 
-	err := setOutputOrder(rq)
+	sorted, ichain, err := getLHInstructionOrder(self.Instructions, nil, self.OutputSort)
 	if encounteredError := err != nil; self.ExpectingError != encounteredError {
 		t.Fatalf("ExpectingError: %t, Encountered Error: %v", self.ExpectingError, err)
 		return

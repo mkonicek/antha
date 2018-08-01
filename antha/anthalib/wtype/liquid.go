2b53e62b997ef4eefafec5b4015721ec1ce04df5
chr1sgrant <chrisrgrant@gmail.com>
2018-08-01 16:22:21 +0100

    deep copy sub components when duplicated a liquid

--- antha/anthalib/wtype/liquid.go
+++ antha/anthalib/wtype/liquid.go
@@ -609,7 +609,10 @@ func (lhc *Liquid) Dup() *Liquid {
 		for k, v := range lhc.Extra {
 			c.Extra[k] = v
 		}
-		c.SubComponents = lhc.SubComponents
+		c.SubComponents.Components = make(map[string]wunit.Concentration, len(lhc.SubComponents.Components))
+		for k, v := range lhc.SubComponents.Components {
+			c.SubComponents.Components[k] = v
+		}
 
 		c.Loc = lhc.Loc
 		c.Destination = lhc.Destination

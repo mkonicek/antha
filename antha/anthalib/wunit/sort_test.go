79787a0e32a495a44fa5286c91d12922b7165f47
Haydn King <h.king@synthace.com>
2018-08-21 16:12:28 +0100

    test sorting

---
+++ antha/anthalib/wunit/sort_test.go
@@ -0,0 +1,195 @@
+package wunit
+
+import (
+	"testing"
+)
+
+// SortTest this is used to test both SortConcentrations and SortVolumes with only
+// a little code duplication
+type SortTest struct {
+	Name  string
+	Input []interface{}
+	Order []int //index of which concentration should be where
+	Error bool  //true if error is expected, in which case Order is ignored
+}
+
+func (self *SortTest) RunConcentrations(t *testing.T) {
+	t.Run(self.Name, func(t *testing.T) {
+		//assumption: sort doesn't modify the original, test will fail if it does
+		input := make([]Concentration, 0, len(self.Input))
+		for _, i := range self.Input {
+			input = append(input, i.(Concentration))
+		}
+		sorted, err := SortConcentrations(input)
+		output := make([]interface{}, 0, len(sorted))
+		for _, i := range sorted {
+			output = append(output, i)
+		}
+		self.Validate(t, output, err)
+	})
+}
+
+func (self *SortTest) RunVolumes(t *testing.T) {
+	t.Run(self.Name, func(t *testing.T) {
+		//assumption: sort doesn't modify the original, test will fail if it does
+		input := make([]Volume, 0, len(self.Input))
+		for _, i := range self.Input {
+			input = append(input, i.(Volume))
+		}
+		sorted, err := SortVolumes(input)
+		output := make([]interface{}, 0, len(sorted))
+		for _, i := range sorted {
+			output = append(output, i)
+		}
+		self.Validate(t, output, err)
+	})
+}
+
+func (self *SortTest) Validate(t *testing.T, sorted []interface{}, err error) {
+	if hasErr := err != nil; self.Error != hasErr {
+		t.Errorf("expected error: %t, got error: %v", self.Error, err)
+		return
+	}
+	if !self.Error {
+		expected := make([]interface{}, 0, len(self.Order))
+		for i := range self.Order {
+			expected = append(expected, self.Input[self.Order[i]])
+		}
+		if len(expected) != len(sorted) {
+			t.Fatalf("expected(%d) and sorted(%d) not the same length", len(expected), len(sorted))
+		}
+		mismatched := []int{}
+		for i := range self.Order {
+			if sorted[i] != expected[i] {
+				mismatched = append(mismatched, i)
+			}
+		}
+		if len(mismatched) > 0 {
+			t.Errorf("sorted list doesn't match expected:\n\te: %v\n\tg: %v", expected, sorted)
+		}
+	}
+}
+
+type SortTests []*SortTest
+
+func (self SortTests) RunConcentrations(t *testing.T) {
+	for _, test := range self {
+		test.RunConcentrations(t)
+	}
+}
+
+func (self SortTests) RunVolumes(t *testing.T) {
+	for _, test := range self {
+		test.RunVolumes(t)
+	}
+}
+func TestSortConcentrations(t *testing.T) {
+	SortTests{
+		{
+			Name: "simple test",
+			Input: []interface{}{
+				NewConcentration(3, "g/l"),
+				NewConcentration(2, "g/l"),
+				NewConcentration(1, "g/l"),
+			},
+			Order: []int{2, 1, 0},
+		},
+		{
+			Name: "already sorted",
+			Input: []interface{}{
+				NewConcentration(1, "g/l"),
+				NewConcentration(2, "g/l"),
+				NewConcentration(3, "g/l"),
+			},
+			Order: []int{0, 1, 2},
+		},
+		{
+			Name: "values equal",
+			Input: []interface{}{
+				NewConcentration(3, "g/l"),
+				NewConcentration(2, "g/l"),
+				NewConcentration(1, "g/l"),
+				NewConcentration(2, "g/l"),
+			},
+			Order: []int{2, 1, 3, 0}, //swapping 1 and 3 permitted
+		},
+		{
+			Name: "different units",
+			Input: []interface{}{
+				NewConcentration(3, "ug/ul"),
+				NewConcentration(2, "mg/ml"),
+				NewConcentration(1, "g/l"),
+			},
+			Order: []int{2, 1, 0},
+		},
+		{
+			Name: "different units affecting order",
+			Input: []interface{}{
+				NewConcentration(3, "ng/ul"),
+				NewConcentration(2, "mg/ml"),
+				NewConcentration(1, "g/l"),
+			},
+			Order: []int{0, 2, 1},
+		},
+		{
+			Name: "invalid units",
+			Input: []interface{}{
+				NewConcentration(3, "ng/ul"),
+				NewConcentration(2, "mg/ml"),
+				NewConcentration(1, "X"),
+			},
+			Error: true,
+		},
+	}.RunConcentrations(t)
+}
+
+func TestSortVolumes(t *testing.T) {
+	SortTests{
+		{
+			Name: "simple test",
+			Input: []interface{}{
+				NewVolume(3, "ul"),
+				NewVolume(2, "ul"),
+				NewVolume(1, "ul"),
+			},
+			Order: []int{2, 1, 0},
+		},
+		{
+			Name: "already sorted",
+			Input: []interface{}{
+				NewVolume(1, "ul"),
+				NewVolume(2, "ul"),
+				NewVolume(3, "ul"),
+			},
+			Order: []int{0, 1, 2},
+		},
+		{
+			Name: "values equal",
+			Input: []interface{}{
+				NewVolume(3, "ul"),
+				NewVolume(2, "ul"),
+				NewVolume(1, "ul"),
+				NewVolume(2, "ul"),
+			},
+			Order: []int{2, 1, 3, 0}, //swapping 1 and 3 permitted
+		},
+		{
+			Name: "different units",
+			Input: []interface{}{
+				NewVolume(3, "nl"),
+				NewVolume(2, "ml"),
+				NewVolume(1, "ul"),
+			},
+			Order: []int{0, 2, 1},
+		},
+		{
+			Name: "invalid units",
+			Input: []interface{}{
+				NewVolume(3, "ul"),
+				NewVolume(2, "mg/ml"),
+				NewVolume(1, "rpm"),
+			},
+			Error: true,
+		},
+	}.RunVolumes(t)
+}

d82505db8988a4df884fac868c52b20bfa4855f3
Haydn King <h.king@synthace.com>
2018-09-14 10:31:11 +0100

    Derive a better model of liquid height

--- inventory/testinventory/serialize/make_plate_library.go
+++ inventory/testinventory/serialize/make_plate_library.go
@@ -663,7 +663,8 @@ func makePCRPlateWell() *wtype.LHWell {
 	pcrplatewell.SetAfVFunc(afs)
 
 	//LiquidLevel model for LL Following: vol_f estimates volume given height
-	vol_f := wutil.Quadratic{A: 0.402, B: 7.069, C: 0.0}
+	//vol_f := wutil.Quadratic{A: 0.402, B: 7.069, C: 0.0}
+	vol_f := wutil.Quadratic{A: 0., B: 200.0 / 8.0, C: 0.}
 	pcrplatewell.SetLiquidLevelModel(vol_f)
 
 	return pcrplatewell

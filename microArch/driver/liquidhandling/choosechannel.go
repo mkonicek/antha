14e1144089054af6a8122e341c260a24558e9ba7
Haydn King <h.king@synthace.com>
2018-12-18 12:51:45 +0000

    Revert "Change tip-head compatibility: only some overlap required"
    This reverts commit 78ea92328c5b1014afcb6cec4e9fee98eade6ea7.

--- microArch/driver/liquidhandling/choosechannel.go
+++ microArch/driver/liquidhandling/choosechannel.go
@@ -140,8 +140,9 @@ func ChooseChannel(vol wunit.Volume, prms *LHProperties) (*wtype.LHChannelParame
 }
 
 func tipHeadCompatible(tip *wtype.LHTip, head *wtype.LHHead) bool {
-	// true if some overlap between head and tip range
-	return tip.MaxVol.GreaterThan(head.Params.Minvol) || tip.MinVol.LessThan(head.Params.Maxvol)
+	//v1 - tip range must be contained entirely within head range
+
+	return !(tip.MinVol.LessThan(head.Params.Minvol) || tip.MaxVol.GreaterThan(head.Params.Maxvol))
 }
 
 func ChooseChannels(vols []wunit.Volume, prms *LHProperties) ([]*wtype.LHChannelParameter, []*wtype.LHTip, []string, error) {

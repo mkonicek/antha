ba4cf1510dd1b793dba5ba7efc3c327ac1462011
Matthew Gregg <m.gregg@synthace.com>
2019-04-11 16:27:11 +0100

    Support maps and arrays for embedded data

--- workflow/v1_2/workflow.go
+++ workflow/v1_2/workflow.go
@@ -3,6 +3,7 @@ package v1_2
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/antha-lang/antha/antha/anthalib/wtype"
@@ -79,19 +80,89 @@ type bakedInFile struct {
 }
 
 func maybeMigrateFileParam(fm *effects.FileManager, param json.RawMessage) (json.RawMessage, error) {
-	bif := &bakedInFile{}
-	if err := json.Unmarshal([]byte(param), bif); err != nil {
-		return param, nil
-	} else if bif.Name == "" && len(bif.Bytes.Bytes) == 0 {
-		return param, nil
+	if success, js, err := maybeMigrateFileFlat(fm, param); err != nil {
+		return nil, err
+	} else if success {
+		return js, nil
+	} else if success, js, err := maybeMigrateFileArray(fm, param); err != nil {
+		return nil, err
+	} else if success {
+		return js, nil
+	} else if success, js, err := maybeMigrateFileMap(fm, param); err != nil {
+		return nil, err
+	} else if success {
+		return js, nil
 	} else {
-		if f, err := fm.WriteAll(bif.Bytes.Bytes, bif.Name); err != nil {
-			return nil, err
-		} else if fbs, err := json.Marshal(f.AsInput()); err != nil {
-			return nil, err
+		return param, nil
+	}
+}
+
+func maybeMigrateFileMap(fm *effects.FileManager, param json.RawMessage) (bool, json.RawMessage, error) {
+	bifMap := make(map[string]bakedInFile, 0)
+	if err := json.Unmarshal([]byte(param), &bifMap); err != nil {
+		return false, nil, nil
+	}
+
+	js := make(map[string]wtype.File, len(bifMap))
+	for k, bif := range bifMap {
+		if msg, err := moveDataToFile(fm, bif); err != nil {
+			return false, nil, err
 		} else {
-			return json.RawMessage(fbs), nil
+			js[k] = *msg
 		}
+	}
+
+	if msg, err := json.Marshal(js); err != nil {
+		return false, nil, err
+	} else {
+		return true, msg, nil
+	}
+}
+
+func maybeMigrateFileArray(fm *effects.FileManager, param json.RawMessage) (bool, json.RawMessage, error) {
+	bifArr := make([]bakedInFile, 0)
+
+	if err := json.Unmarshal([]byte(param), &bifArr); err != nil {
+		return false, nil, nil
+	}
+
+	fmt.Printf("This is an array! %v\n", bifArr)
+	js := make([]wtype.File, len(bifArr))
+	for i, bif := range bifArr {
+		if msg, err := moveDataToFile(fm, bif); err != nil {
+			return false, nil, err
+		} else {
+			js[i] = *msg
+		}
+	}
+
+	if msg, err := json.Marshal(js); err != nil {
+		return false, nil, err
+	} else {
+		return true, msg, nil
+	}
+}
+
+func maybeMigrateFileFlat(fm *effects.FileManager, param json.RawMessage) (bool, json.RawMessage, error) {
+	bif := bakedInFile{}
+	if err := json.Unmarshal([]byte(param), &bif); err != nil {
+		return false, param, nil
+	} else if bif.Name == "" && len(bif.Bytes.Bytes) == 0 {
+		return false, param, nil
+	} else if f, err := moveDataToFile(fm, bif); err != nil {
+		return false, nil, err
+	} else if msg, err := json.Marshal(f); err != nil {
+		return false, nil, err
+	} else {
+		return true, msg, nil
+	}
+}
+
+func moveDataToFile(fm *effects.FileManager, bif bakedInFile) (*wtype.File, error) {
+	if f, err := fm.WriteAll(bif.Bytes.Bytes, bif.Name); err != nil {
+		return nil, err
+	} else {
+		return f.AsInput(), nil
 	}
 }
 

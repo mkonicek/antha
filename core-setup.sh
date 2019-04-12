9ffdc0e30b804933a50d27bc792e28846df1bbb2
Matthew Sackman <m.sackman@synthace.com>
2019-04-12 15:24:17 +0100

    [AC-25] Try harder.

--- core-setup.sh
+++ core-setup.sh
@@ -3,6 +3,6 @@
 set -ex
 repos="github.com/Synthace/instruction-plugins github.com/Synthace/antha-runner"
 for r in $repos; do
-    go get $r
-    ( cd /go/src/$r && git fetch --all && git checkout feature/future_sanity )
+    mkdir /go/src/$r
+    ( cd /go/src/$r && git clone https://$r . && git checkout feature/future_sanity )
 done

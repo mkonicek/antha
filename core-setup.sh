c6ab40ced00dd4613f51b5053272d67ffce10b32
Matthew Sackman <m.sackman@synthace.com>
2019-04-12 15:26:26 +0100

    [AC-25] GRRRR

--- core-setup.sh
+++ core-setup.sh
@@ -3,6 +3,6 @@
 set -ex
 repos="github.com/Synthace/instruction-plugins github.com/Synthace/antha-runner"
 for r in $repos; do
-    mkdir /go/src/$r
+    mkdir -p /go/src/$r
     ( cd /go/src/$r && git clone https://$r . && git checkout feature/future_sanity )
 done

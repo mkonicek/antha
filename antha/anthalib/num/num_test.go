6e989205ed792b1fe7187465455ad6654e556187
Lev Itckovich <45764739+levitskovich@users.noreply.github.com>
2019-04-17 11:47:15 +0100

    ANTHA-2641: `num` - a library of float arithmetic functions of general use which Antha users might need (#1037)
    * ANTHA-2641: `num` - a library of float arithmetic functions of general use which Antha users might need
    
    For now, `num` contains:
    1. Wrappers on the top of Add, Sub, Mul, Div functions from `gonum`. The point is that `gonum` doesn't have:
    * functions like `Add(v1 []float64, v2 []float64) []float64` (there only their versions which require an output vector as well, which are not very readable)
    * functions like `Add(v1 []float64, scalar float64) []float64`
    2. Several functions from `numpy` - e.g. Linspace and Convolve.
    
    Considered adding more element-wise functions (e.g. `Exp(v)` and `Pow(v1, v2)`), but not sure it makes any sense.
    Complex formulas like `y = exp(-1 / (1 - x^2)))` look absolutely unreadable in this syntax:
    `y = num.Exp(num.DivConst(-1, num.SubFromConst(1, num.Pow(x, 2))))`
    So looks like it's easier to do things like this just in a for loop.
    
    * Minor fixes:
    * assertPanics -> assert.Panics (using "github.com/stretchr/testify/assert")
    * ConvolutionMode, Convolve: comments
    * Convolve: fixing inputs check
    
    * typo fix

---
+++ antha/anthalib/num/num_test.go
@@ -0,0 +1,154 @@
+package num
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gonum.org/v1/gonum/floats"
+)
+
+func TestAdd(t *testing.T) {
+	// Add
+	actual := Add([]float64{1, 2, 3}, []float64{4, 5, 6})
+	expected := []float64{5, 7, 9}
+	assertEqual(t, expected, actual, "Add")
+
+	// Add - wrong inputs sizes
+	assert.Panics(t, func() {
+		Add([]float64{1, 2, 3}, []float64{1, 2})
+	}, "Add - wrong inputs sizes")
+
+	// AddConst
+	actual = AddConst([]float64{1, 2, 3}, 1)
+	expected = []float64{2, 3, 4}
+	assertEqual(t, expected, actual, "AddConst")
+}
+
+func TestSub(t *testing.T) {
+	// Sub
+	actual := Sub([]float64{1, 2, 3}, []float64{4, 5, 6})
+	expected := []float64{-3, -3, -3}
+	assertEqual(t, expected, actual, "Sub")
+
+	// Sub - wrong inputs sizes
+	assert.Panics(t, func() {
+		Sub([]float64{1, 2, 3}, []float64{1, 2})
+	}, "Sub - wrong inputs sizes")
+
+	// SubConst
+	actual = SubConst([]float64{1, 2, 3}, 1)
+	expected = []float64{0, 1, 2}
+	assertEqual(t, expected, actual, "SubConst")
+
+	// SubFromConst
+	actual = SubFromConst(1, []float64{1, 2, 3})
+	expected = []float64{0, -1, -2}
+	assertEqual(t, expected, actual, "SubFromConst")
+}
+
+func TestMul(t *testing.T) {
+	// Mul
+	actual := Mul([]float64{1, 2, 3}, []float64{4, 5, 6})
+	expected := []float64{4, 10, 18}
+	assertEqual(t, expected, actual, "Mul")
+
+	// Mul - wrong inputs sizes
+	assert.Panics(t, func() {
+		Mul([]float64{1, 2, 3}, []float64{1, 2})
+	}, "Mul - wrong inputs sizes")
+
+	// MulByConst
+	actual = MulByConst([]float64{1, 2, 3}, 2)
+	expected = []float64{2, 4, 6}
+	assertEqual(t, expected, actual, "MulByConst")
+}
+
+func TestDiv(t *testing.T) {
+	// Div
+	actual := Div([]float64{4, 10, 24}, []float64{4, 5, 6})
+	expected := []float64{1, 2, 4}
+	assertEqual(t, expected, actual, "Div")
+
+	// Div - wrong inputs sizes
+	assert.Panics(t, func() {
+		Div([]float64{1, 2, 3}, []float64{1, 2})
+	}, "Sub - wrong inputs sizes")
+
+	// DivConst
+	actual = DivConst(6, []float64{1, 2, 3})
+	expected = []float64{6, 3, 2}
+	assertEqual(t, expected, actual, "DivConst")
+
+	// DivByConst
+	actual = DivByConst([]float64{2, 4, 6}, 2)
+	expected = []float64{1, 2, 3}
+	assertEqual(t, expected, actual, "DivByConst")
+}
+
+func TestZeroes(t *testing.T) {
+	// Zeroes
+	actual := Zeroes(3)
+	expected := []float64{0, 0, 0}
+	assertEqual(t, expected, actual, "Zeroes")
+
+	// Zeroes - negative size
+	assert.Panics(t, func() {
+		_ = Zeroes(-5)
+	}, "Zeroes - negative size")
+}
+
+func TestOnes(t *testing.T) {
+	// Ones
+	actual := Ones(3)
+	expected := []float64{1, 1, 1}
+	assertEqual(t, expected, actual, "Ones")
+
+	// Ones - negative size
+	assert.Panics(t, func() {
+		_ = Ones(-5)
+	}, "Ones - negative size")
+}
+
+func TestLinspace(t *testing.T) {
+	// Linspace
+	actual := Linspace(1, 4, 4)
+	expected := []float64{1, 2, 3, 4}
+	assertEqual(t, expected, actual, "Linspace")
+
+	// Linspace - negative steps count
+	assert.Panics(t, func() {
+		_ = Linspace(1, 4, -4)
+	}, "Linspace - negative steps count")
+}
+
+func TestConvolve(t *testing.T) {
+	// Tests from https://docs.scipy.org/doc/numpy/reference/generated/numpy.convolve.html
+	actual := Convolve([]float64{1, 2, 3}, []float64{0, 1, 0.5}, Full)
+	expected := []float64{0., 1., 2.5, 4., 1.5}
+	assertEqual(t, expected, actual, "Convolve (full)")
+
+	actual = Convolve([]float64{1, 2, 3}, []float64{0, 1, 0.5}, Same)
+	expected = []float64{1., 2.5, 4.}
+	assertEqual(t, expected, actual, "Convolve (same)")
+
+	actual = Convolve([]float64{1, 2, 3}, []float64{0, 1, 0.5}, Valid)
+	expected = []float64{2.5}
+	assertEqual(t, expected, actual, "Convolve (valid)")
+
+	// Convolve - empty input
+	assert.Panics(t, func() {
+		_ = Convolve([]float64{}, []float64{1}, Full)
+	}, "Convolve - empty input")
+	assert.Panics(t, func() {
+		_ = Convolve([]float64{1}, []float64{}, Full)
+	}, "Convolve - empty input")
+}
+
+const eqTolerance = 1e-14
+
+// Compares two float vectors using a hard-coded max discrepancy value.
+func assertEqual(t *testing.T, expected, actual []float64, msg string) {
+	if !floats.EqualApprox(expected, actual, eqTolerance) {
+		t.Errorf(msg+": expected %v, actual %v", expected, actual)
+	}
+}
---
+++ antha/anthalib/num/numpy.go
@@ -0,0 +1,97 @@
+package num
+
+import (
+	"github.com/pkg/errors"
+)
+
+// Implementations of some useful numpy functions which are not available in gonum.
+
+// Zeroes returns an array of zeroes of specified size.
+// It's encouraged to use it instead of just make() in case the further code relies on the fact that the array contains zeroes.
+func Zeroes(size int) []float64 {
+	return make([]float64, size)
+}
+
+// Ones return an array of ones of specified size.
+func Ones(size int) []float64 {
+	result := make([]float64, size)
+	for i := range result {
+		result[i] = 1
+	}
+	return result
+}
+
+// Linspace splits the interval [start, end] into `num - 1` equal intervals and returns `num` split points.
+func Linspace(start, end float64, num int) []float64 {
+	if num < 0 {
+		panic(errors.Errorf("number of samples, %d, must be non-negative.", num))
+	}
+	result := make([]float64, num)
+	step := (end - start) / float64(num-1)
+	for i := range result {
+		result[i] = start + float64(i)*step
+	}
+	return result
+}
+
+// ConvolutionMode defines convolution output array length.
+type ConvolutionMode int
+
+const (
+	// Full - returns the convolution at each point of overlap, i.e. of length N+M-1.
+	Full = iota
+	// Same - returns the output of length max(M, N).
+	Same
+	// Valid - returns the output of length max(M, N) - min(M, N) + 1.
+	Valid
+)
+
+// Convolve is a (very naive) implementation of precise discrete convolution.
+// The results are numerically equivalent to `np.convolve(a, v, mode)` - it looks like that `np.convolve` uses precise convolution as well (but not an FFT approximation).
+// TODO: optimize the implementation - the current one has O((M+N)^2) time complexity. Looks like it's possible to achieve at least O(MN).
+func Convolve(a, v []float64, mode ConvolutionMode) []float64 {
+	if len(a) == 0 {
+		panic(errors.New("Convolve: a cannot be empty"))
+	}
+	if len(v) == 0 {
+		panic(errors.New("Convolve: v cannot be empty"))
+	}
+
+	// the code below relies on the fact that `a` is the longer array
+	if len(v) > len(a) {
+		a, v = v, a
+	}
+
+	size := len(a) + len(v) - 1
+
+	// a + zeroes
+	a_ext := Zeroes(size)
+	copy(a_ext[:len(a)], a)
+
+	// v + zeroes
+	v_ext := Zeroes(size)
+	copy(v_ext[:len(v)], v)
+
+	result := Zeroes(size)
+	for i := 0; i < size; i++ {
+		for j := 0; j < size-i; j++ {
+			result[i+j] += a_ext[i] * v_ext[j]
+		}
+	}
+
+	switch mode {
+	case Full:
+		// `Full` mode: returning the whole result
+		return result
+	case Same:
+		// `Same` mode: returning the subarray of length `len(a)`
+		toCut := len(v) / 2 // is this correct? at least, this works for the sample data
+		return result[toCut : toCut+len(a)]
+	case Valid:
+		// `Valid` mode: returning the subarray of length `len(a) - len(v) + 1`
+		toCut := len(v) - 1
+		return result[toCut:len(a)]
+	default:
+		panic(errors.Errorf("invalid convolution mode %v", mode))
+	}
+}

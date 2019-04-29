59e8cddb2c1e968e0a92a9d5eea0e31a564127e3
Matthew Sackman <m.sackman@synthace.com>
2019-04-29 14:13:06 +0100

    [AC-61] Try to upload to coveralls.
    But I have a suspicion that the github.com/mattn/goveralls code may fail because I wonder whether the stdlib go/build stuff is module aware yet.

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,17 @@
 FROM eu.gcr.io/antha-images/golang:1.12.4-build
 
 ARG COMMIT_SHA
-ADD .netrc /
-RUN mv /.netrc $HOME/.netrc || true
+ADD .netrc .coveralls_token /
+RUN mv /.netrc /.coveralls_token $HOME/
 RUN mkdir /antha
 WORKDIR /antha
+RUN set -ex && go get -u github.com/mattn/goveralls
 RUN set -ex && go mod init antha && go get github.com/antha-lang/antha@$COMMIT_SHA && go mod download
 RUN set -ex && go install github.com/antha-lang/antha/cmd/...
 RUN set -ex && go test -c github.com/antha-lang/antha/cmd/elements
 COPY scripts/. /antha/.
 RUN ./antha-test.sh
-RUN rm $HOME/.netrc
+RUN rm $HOME/.netrc $HOME/.coveralls_token
 
 # These are for the gitlab CI for elements:
 ONBUILD ADD . /elements
--- scripts/antha-test.sh
+++ scripts/antha-test.sh
@@ -1,11 +1,18 @@
 #! /bin/sh
 
 ## This script is run by cloudbuild as part of the CI for antha itself.
-set -e
+set -o nounset -o errexit -o pipefail -o noclobber
+shopt -s failglob
 
 ## There are some packages that only contain test files. Go test gets
 ## upset if you try to include these packages in coverage, so we have
 ## to filter them out:
 COVERPKG=$(go list -f '{{if (len .GoFiles) gt 0}}{{.ImportPath}}{{end}}' github.com/antha-lang/antha/... | tr '\n' ',' | sed -e 's/,$//')
 
-exec go test -covermode=atomic -coverprofile=cover.out -coverpkg="${COVERPKG}" github.com/antha-lang/antha/...
+go test -covermode=atomic -coverprofile=cover.out -coverpkg="${COVERPKG}" github.com/antha-lang/antha/...
+
+COVERALLS_TOKEN_FILE=${HOME}/.coveralls_token
+if [[ -f ${COVERALLS_TOKEN_FILE} && -f cover.out ]]; then
+    COVERALLS_TOKEN=$(<${COVERALLS_TOKEN_FILE})
+    goveralls -repotoken=${COVERALLS_TOKEN} -coverprofile=cover.out -package github.com/antha-lang/antha
+fi

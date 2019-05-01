d59bfe32523e04ac202a6358e326dc7765a18a8c
Simon Whitaker <simon@whitakersolutions.co.uk>
2019-05-01 14:17:32 +0100

    Add skeleton for v1.2 provider

---
+++ workflow/v1_2/provider.go
@@ -0,0 +1,33 @@
+package v1_2
+
+import (
+	"github.com/antha-lang/antha/workflow"
+)
+
+type V1_2WorkflowProvider struct {
+	owf *workflowv1_2
+}
+
+func (p *V1_2WorkflowProvider) GetMeta() (*workflow.Meta, error) {
+	return nil, nil
+}
+
+func (p *V1_2WorkflowProvider) GetRepositories() (*workflow.Repositories, error) {
+	return nil, nil
+}
+
+func (p *V1_2WorkflowProvider) GetElements() (*workflow.Elements, error) {
+	return nil, nil
+}
+
+func (p *V1_2WorkflowProvider) GetInventory() (*workflow.Inventory, error) {
+	return nil, nil
+}
+
+func (p *V1_2WorkflowProvider) GetConfig() (*workflow.Config, error) {
+	return nil, nil
+}
+
+func (p *V1_2WorkflowProvider) GetTesting() (*workflow.Testing, error) {
+	return nil, nil
+}

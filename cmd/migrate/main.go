f35dfa1caa92880fda56b6bd041df25967447054
Simon Whitaker <simon@whitakersolutions.co.uk>
2019-05-02 11:42:44 +0100

    Update main.go to use the new migrator

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -2,51 +2,115 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
+	"github.com/antha-lang/antha/laboratory/effects"
 	"github.com/antha-lang/antha/logger"
 	"github.com/antha-lang/antha/workflow"
+	"github.com/antha-lang/antha/workflow/migrate"
+	"github.com/antha-lang/antha/workflow/migrate/provider"
 	"github.com/antha-lang/antha/workflow/v1_2"
 )
+
+const (
+	fromFormatJSON     = "json"
+	fromFormatProtobuf = "protobuf"
+)
+
+var validFromFormats = []string{fromFormatJSON, fromFormatProtobuf}
 
 func main() {
 	flag.Usage = workflow.NewFlagUsage(nil, "Migrate workflow to latest schema version")
 
-	var fromFile, outDir, gilsonDevice string
+	var fromFile, outDir, gilsonDevice, fromFormat string
 	var validate bool
 	flag.StringVar(&outDir, "outdir", "", "Directory to write to (default: a temporary directory will be created)")
 	flag.StringVar(&fromFile, "from", "-", "File to migrate from (default: will be read from stdin)")
+	flag.StringVar(&fromFormat, "format", fromFormatJSON, fmt.Sprintf("Format of the from file. One of: %v", validFromFormats))
 	flag.StringVar(&gilsonDevice, "gilson-device", "", "A gilson device name to use for migrated config. If not present, device specific configuration will not be migrated.")
 	flag.BoolVar(&validate, "validate", true, "Validate input and output files.")
 	flag.Parse()
 
 	l := logger.NewLogger()
 
-	m, err := v1_2.NewMigrater(l, flag.Args(), fromFile, outDir, gilsonDevice)
+	// Read in the workflow snippets
+	rs, err := workflow.ReadersFromPaths(flag.Args())
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	cwf, err := workflow.WorkflowFromReaders(rs...)
 	if err != nil {
 		logger.Fatal(l, err)
 	}
 
-	if err := m.ValidateOld(); err != nil {
-		if validate {
-			logger.Fatal(l, err)
-		} else {
-			l.Log("OriginalFileValidationError", err)
+	// Get the repo map
+	repoMap, err := cwf.Repositories.FindAllElementTypes()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	// Prepare the output directory
+	if outDir == "" {
+		if outDir, err = ioutil.TempDir("", "antha-migrater"); err != nil {
+			logger.Fatal(err)
+		}
+	}
+	for _, leaf := range []string{"workflow", "data"} {
+		if err := os.MkdirAll(filepath.Join(outDir, leaf), 0700); err != nil {
+			logger.Fatal(err)
 		}
 	}
 
-	if err := m.MigrateAll(); err != nil {
-		logger.Fatal(l, err)
+	dataDir := filepath.Join(outDir, "data")
+	fm, err := effects.NewFileManager(dataDir, dataDir)
+	if err != nil {
+		logger.Fatal(err)
 	}
 
-	if err := m.ValidateCur(); err != nil {
-		if validate {
-			logger.Fatal(l, err)
-		} else {
-			l.Log("ValidationError", err)
+	logger.Log("outdir", outDir)
+
+	var provider provider.WorkflowProvider
+	switch fromFormat {
+	case fromFormatJSON:
+		{
+			provider, err = v1_2.NewV1_2WorkflowProvider(fromFile, fm, repoMap, gilsonDevice, logger)
+			if err != nil {
+				logger.Fatal(err)
+			}
+		}
+	case fromFormatProtobuf:
+		{
+			logger.Fatal(fmt.Errorf("Format not implemented"))
+		}
+	default:
+		{
+			logger.Fatal(fmt.Errorf("Unknown format '%v', valid formats are: %v", fromFormat, validFromFormats))
 		}
 	}
 
-	if err := m.SaveCur(); err != nil {
-		logger.Fatal(l, err)
+	m := migrate.NewMigrator(logger, provider)
+	wf, err := m.Workflow()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	// Merge the generated v2.0 workflow into the stuff we got from the snippets
+	if err = cwf.Merge(wf); err != nil {
+		logger.Fatal(err)
+	}
+
+	// validate the resulting workflow
+	if err = cwf.Validate(); err != nil {
+		logger.Fatal(err)
+	}
+
+	// Save to disk
+	p := filepath.Join(outDir, "workflow", "workflow.json")
+	if err = cwf.WriteToFile(p, true); err != nil {
+		logger.Fatal(err)
 	}
 }

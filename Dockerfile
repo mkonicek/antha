c419b1accfcbfe23ff4ef740c056adb7a099e6a7
Matthew Sackman <m.sackman@synthace.com>
2019-05-03 13:50:20 +0100

    Cherry pick fixes from matthew/frankenstein
    Fixes for:
    Dockerfile: correctly warm build caches
    compile.go: use -mod=readonly for calls to go build
    composer-cloud.sh: correct paths

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,8 @@ RUN printf "%s\n" "$NETRC" > $HOME/.netrc
 RUN mkdir /antha
 WORKDIR /antha
 RUN set -ex && go mod init antha && go mod edit "-require=github.com/antha-lang/antha@$COMMIT_SHA" && go mod download
-RUN set -ex && go install github.com/antha-lang/antha/cmd/...
+# Do these builds to pre-warm the build cache. This makes a HUGE difference to performance in the cloud
+RUN set -ex && go build github.com/antha-lang/antha/... github.com/Synthace/antha-runner/... github.com/Synthace/instruction-plugins/... && go install github.com/antha-lang/antha/cmd/...
 RUN set -ex && go test -c github.com/antha-lang/antha/cmd/elements
 COPY scripts/. /antha/.
 RUN ./antha-test.sh
--- scripts/composer-cloud.sh
+++ scripts/composer-cloud.sh
@@ -12,8 +12,10 @@ DATA_DIR=${DATA_DIR:-/data}
 # (See https://github.com/Synthace/microservice/tree/master/cmd/ambassador )
 { while ! [[ -r $DATA_DIR/inputReady ]]; do sleep 0.1; done; }
 < $DATA_DIR/inputReady
-trap "{ > $DATA_DIR/outputReady; }" EXIT
+trap "{ echo > $DATA_DIR/outputReady; }" EXIT
 
-$MIGRATE -from=${DATA_DIR}/input/request.pb -outdir=${DATA_DIR}/scratch -gilson-device=gillian -format=protobuf - <<<$WF_JSON
+$MIGRATE -from=${DATA_DIR}/input/workflow/request.pb -outdir=${DATA_DIR}/scratch -gilson-device=gillian -format=protobuf - <<<$WF_JSON
+
+cp -a ${DATA_DIR}/scratch/* ${DATA_DIR}/input
 
 $COMPOSER -indir=${DATA_DIR}/scratch -outdir=${DATA_DIR}/output -linkedDrivers

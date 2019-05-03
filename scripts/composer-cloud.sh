ef7cd3491050ced41eba48a98f837b3535259b3f
Matthew Sackman <m.sackman@synthace.com>
2019-05-03 11:58:57 +0100

    Gah, remember that find takes path first.

--- scripts/composer-cloud.sh
+++ scripts/composer-cloud.sh
@@ -15,16 +15,16 @@ DATA_DIR=${DATA_DIR:-/data}
 trap "{ > $DATA_DIR/outputReady; }" EXIT
 
 echo input
-find -type f ${DATA_DIR}/input
+find ${DATA_DIR}/input -type f
 $MIGRATE -from=${DATA_DIR}/input/workflow/request.pb -outdir=${DATA_DIR}/scratch -gilson-device=gillian -format=protobuf - <<<$WF_JSON
 
 echo scratch
-find -type f ${DATA_DIR}/scratch
+find ${DATA_DIR}/scratch -type f
 cp -a ${DATA_DIR}/scratch/* ${DATA_DIR}/input
 
 echo input
-find -type f ${DATA_DIR}/input
+find ${DATA_DIR}/input -type f
 $COMPOSER -indir=${DATA_DIR}/scratch -outdir=${DATA_DIR}/output -linkedDrivers
 
 echo output
-find -type f ${DATA_DIR}/output
+find ${DATA_DIR}/output -type f

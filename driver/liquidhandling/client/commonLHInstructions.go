1eacb4126b4ac5268e20d83f02f4c84ef075f412
Arne Vandenbroucke <a.vandenbroucke@synthace.com>
2019-05-04 16:21:50 +0100

    Changed prototype of GetAllowedLocations
    Summary:
    In order to differentiate between no locations found and no implementation I needed to return nil by default for the passthrough. The function arguments are also different: no need to pass the allowed string array to the input
    
    Tested:
    code compiles

--- driver/liquidhandling/client/commonLHInstructions.go
+++ driver/liquidhandling/client/commonLHInstructions.go
@@ -9,7 +9,7 @@ type CommonLHInstructions struct{}
 
 // method common to all liquidhandling instruction drivers - embed in all dependent methods and reimplement as needed
 // https://medium.com/@simplyianm/why-gos-structs-are-superior-to-class-based-inheritance-b661ba897c67 !!
-func (c *CommonLHInstructions) GetAllowedLocations(plate *wtype.Plate, allowed []string) ([]string, driver.CommandStatus) {
+func (c *CommonLHInstructions) GetAllowedLocations(plate *wtype.Plate) ([]string, driver.CommandStatus) {
 	//self.AddWarning("Pass Through")
-	return allowed, driver.CommandOk()
+	return nil, driver.CommandOk()
 }
